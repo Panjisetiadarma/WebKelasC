e8e8687221265fac2996ca85e574765e4e149d3f
Panjisetiadarma <setiadarmapanji@gmail.com>
2025-06-14 22:16:17 +0700

    update
    input image profile sesuai nim dan tambahan link portofolio

--- WebClass_git/script.js
+++ WebClass_git/script.js
@@ -170,181 +170,301 @@
                 nim: "2450081061",
                 name: "DAMADIKA ROBIKURNIA",
                 ttl: "Bandung, 12 Januari 2006",
-                bio: "Penggemar teknologi, suka eksplorasi UI/UX."
+                bio: "Penggemar teknologi, suka eksplorasi UI/UX.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081062",
                 name: "M. SULTHAN ARIQ ATHALLA",
                 ttl: "Jakarta, 14 September 2003",
-                bio: "Mewing dulu gak sih"
+                bio: "Mewing dulu gak sih",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081063",
                 name: "RAFLY ANGGARA PUTRA",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081064",
-                name: "S.SANSAN ZAHRA JULLIA AMIN",
+                name: "S.SANSAN ZAHRA JULLIA .A",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081065",
                 name: "NABILA DWI MARSONO",
                 ttl: "Bogor, 27 Januari 2006",
-                bio: "say the name"
+                bio: "say the name",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081066",
                 name: "FAWAZ AZMI ADZDZIKRI",
                 ttl: "Bandung, 19 September 2006",
-                bio: "yang penting hidup."
+                bio: "yang penting hidup.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081067",
                 name: "FANI AULIA DIVAYANTI",
                 ttl: "purwakarta 16 juli 2005",
-                bio:"You are stronger than you think."
+                bio:"You are stronger than you think.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081068",
                 name: "RAZIQ RABBANI SUPARDI",
                 ttl: "Bandung, 17 Mei 2006",
-                bio: "aku tampan banget kata bunda."
+                bio: "aku tampan banget kata bunda.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081069",
                 name: "RIDO ALAN ALJAUHARI",
                 ttl: "Kuningan, 27 Juli 2005",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081070",
                 name: "VINA LUCITA HARAHAP",
                 ttl: "Huta Tunggal, 9 Maret 2006",
-                bio: "If you can dream it, you can do it."
+                bio: "If you can dream it, you can do it.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081071",
                 name: "KHANSA HUMAIRA SUGIHARTO",
                 ttl: "Bekasi, 14 Agustus 2006",
-                bio: "99.9%"
+                bio: "99.9%",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081072",
                 name: "KEVIN CAHYAWIGUNA",
-                ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                ttl: "Cimahi, 1 Januari 2007",
+                bio: "tetaplah jadi pemalas",
+                ig : "https://www.instagram.com/kepincahya/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081073",
                 name: "M.ARYA PERDANA NATAWIRIA",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081074",
                 name: "M. RIZAL SOBARI",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081075",
                 name: "FEBRIAN FIRMANSYAH",
                 ttl: "Bandung, 2 Februari 2007",
-                bio: "Cowo Nyalse"
+                bio: "Cowo Nyalse",
+                ig : "https://www.instagram.com/pepvri/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081076",
                 name: "INSAN NAZIB",
                 ttl: "Sumedang, 3 July 2006",
-                bio: "Mahasiswa TI yang ingin menguasai cyber security."
+                bio: "Mahasiswa TI yang ingin menguasai cyber security.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081077",
                 name: "MUH. ZACKY YASIR SYIFA",
                 ttl: "Makassar, 25 Desember",
-                bio: "Omke Gams Omke Gams"
+                bio: "Omke Gams Omke Gams",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081078",
                 name: "RAFI SAPUTRA",
                 ttl: "Jakarta, 19 Maret 2003",
-                bio: "Pecinta Mie Ayam"
+                bio: "Pecinta Mie Ayam",
+                ig : "https://www.instagram.com/rafi_saputra2005/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081079",
                 name: "AHMAD RAFI NASTI",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081080",
                 name: "M. FARIELYABI GHANI",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081081",
                 name: "DIDEN DIKI GUSTIAN",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081082",
                 name: "ALFA HAEDAR ALI",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081083",
                 name: "SITI AULIA RAHMAN",
                 ttl: "Sukabumi, 3 Juni 2006",
-                bio: "everything happens for reason"
+                bio: "everything happens for reason",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081084",
                 name: "NAILA PUTRI RAMADHANI",
                 ttl: "Bandung, 5 Oktober 2006",
-                bio: "everything be okay"
+                bio: "everything be okay",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081085",
                 name: "REXA ARFA ANDIKA .D",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081086",
                 name: "RICHARDI SIHOTANG",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081087",
                 name: "Panji Setiadarma",
                 ttl: "Karawang, 22 February 2006",
-                bio: "variable tanpa value."
+                bio: "variable tanpa value.",
+                ig : "https://www.instagram.com/_ujiw._/", 
+                git: "https://github.com/Panjisetiadarma/",
+                in : "https://www.linkedin.com/in/panji-setiadarma-27b57536b/",
+                porto : "../MyPorto/index.html"
             },
             {
                 nim: "2450081088",
                 name: "REQI FAUZAN",
                 ttl: "Karawang, 01 April 2005",
-                bio: " Absolutely Cinema 🖐🏻😧🖐🏻"
+                bio: " Absolutely Cinema 🖐🏻😧🖐🏻",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081089",
                 name: "FAISAL AKBAR WIDYATNA",
                 ttl: "Cianjur, 10 Mei 2005",
-                bio: "mending sare"
+                bio: "mending sare",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
             {
                 nim: "2450081090",
                 name: "M. RIDWAN FAUZI",
                 ttl: "Jakarta, 3 Maret 2006",
-                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika."
+                bio: "Mahasiswa TI yang aktif di bidang AI dan robotika.",
+                ig : "https://www.instagram.com/",
+                git: "https://github.com/",
+                in : "https://www.linkin.com/",
+                porto : ""
             },
         ];
         
@@ -358,13 +478,16 @@
         // Fungsi untuk membuat kartu mahasiswa
         function createStudentCard(student) {
             const formattedName = formatName(student.name);
+            const img = new Image();
+            img.src = `images/${student.nim}.jpg`;
+            img.onerror = () => console.error("GAGAL LOAD: ", student.nim); 
 
             return `
                 <div class="student-card" data-name="${formattedName.toLowerCase()}" data-nim="${student.nim}">
                     <div class="student-inner">
                         <div class="student-front">
                             <div class="student-avatar">
-                                <i class="fas fa-user"></i>
+                                <img src="images/${student.nim}.png" alt="${formattedName}" />
                             </div>
                             <h3 class="student-name">${formattedName}</h3>
                             <p class="student-Nim">${student.nim}</p>
@@ -376,9 +499,12 @@
                                 <p><strong>TTL:</strong> ${student.ttl}</p>
                                 <p class="student-bio">${student.bio}</p>
                                 <div class="social-links">
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                    <a href="#"><i class="fab fa-github"></i></a>
-                                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                                    <a href="${student.ig}"><i class="fab fa-instagram"></i></a>
+                                    <a href="${student.git}"><i class="fab fa-github"></i></a>
+                                    <a href="${student.in}"><i class="fab fa-linkedin"></i></a>
+                                    <a href="${student.porto}" target="_blank" class="portfolio-link">
+                                        <i class="fas fa-folder-open"></i>
+                                    </a>
                                 </div>
                             </div>
                             <div class="flip-back">
@@ -389,7 +515,6 @@
                 </div>
             `;
         }
-
 
         // Fungsi untuk menampilkan semua kartu mahasiswa
         function renderStudents() {
